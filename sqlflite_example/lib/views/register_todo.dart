f64a0b2bdf73af9afa4a5eb2da2ad72b1afcc88e
kim.c_cha.nam <144766297+kmh5038@users.noreply.github.com>
2025-03-13 11:11:36 +0900

    feat: Go Router를 사용한 화면 전환 구현
    - Go Router 라이브러리를 사용하여 화면 간 이동 로직 구현

---
+++ sqlflite_example/lib/views/register_todo.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class RegisterTodo extends StatelessWidget {
+  const RegisterTodo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('view2'),
+    );
+  }
+
+}

89dcf9194e334f7afcf5e68b8e0f235d7d08cbfc
kim.c_cha.nam <144766297+kmh5038@users.noreply.github.com>
2025-04-01 18:08:19 +0900

    fix(filmstack): 프로젝트 변경

--- SQLlite_example/lib/my_router.dart
+++
@@ -1,8 +0,0 @@
-import 'package:go_router/go_router.dart';
-import 'package:sqlflite_example/views/home.dart';
-import 'package:sqlflite_example/views/register_todo.dart';
-
-final GoRouter myRouter = GoRouter(initialLocation: '/', routes: [
-  GoRoute(path: '/', builder: (context, state) => Home()),
-  GoRoute(path: '/RegisterTodo', builder: (context, state) => RegisterTodo())
-]);
---
+++ filmstack/android/app/src/main/kotlin/com/example/filmstack/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.filmstack
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()

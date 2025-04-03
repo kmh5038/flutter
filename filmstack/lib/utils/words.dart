a7f99f02cee9cf5ac5fced63a82350050f3a8313
kim.c_cha.nam <144766297+kmh5038@users.noreply.github.com>
2025-04-03 11:11:07 +0900

    [Feat] 초기세팅

---
+++ filmstack/lib/utils/words.dart
@@ -0,0 +1,9 @@
+class Words {
+  Words._();
+
+  static const String homeTitle = "상영중 영화";
+  static const String bottomNavBtn1 = "상영중 영화";
+  static const String bottomNavBtn2 = "필름 스택";
+  static const String bottomNavBtn3 = "프로필";
+
+}
---
+++ filmstack/lib/views/wigets/movie_ranking_section.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class MovieRankingSection extends StatelessWidget {
+    const MovieRankingSection({super.key});
+
+    @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Text('1'),
+        ],
+      ),
+    );
+  }
+}

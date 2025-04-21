6ab7c9b4dd82807c8aa0b1663f7eb6204863dbf2
kim.c_cha.nam <144766297+kmh5038@users.noreply.github.com>
2025-04-21 19:54:35 +0900

    feat(filmstack): API키 숨기기

--- filmstack/lib/main.dart
+++ filmstack/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:filmstack/views/screens/home.dart';
-
-void main() {
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();  
+  await dotenv.load(fileName: ".env");    
   runApp(const MyApp());
 }
 
